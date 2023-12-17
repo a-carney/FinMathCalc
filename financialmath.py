b07a28fd22fa4762b20f88347e23d963a7e46b2d
a-carney <77558389+a-carney@users.noreply.github.com>
2023-12-17 14:28:19 -0500

    refactor
    changed code to use OOP

--- financialmath.py
+++ financialmath.py
@@ -1,92 +1,72 @@
 import math
 import errno
-"""
-Based on the study material for the actuarial FM exam. 
 
-I would like to create a simple command line utility using python to calculate the following:
-1. Time value of money
-2. Annuities
-3. Loans
-4. Bonds
-5. Yield rates
-6. Duration
-7. Immunization
-8. Options
-9. Swaps
-10. Forwards and Futures
-11. Interest rate derivatives
-12. Financial analysis
-"""
+class TVM:
+    def __init__(self, i=None, PV=None, PMT=None, FV=None, n=None):
+        self.i = i
+        self.PV = PV
+        self.PMT = PMT
+        self.FV = FV
+        self.n = n
 
+    def solve_for_pv(self):
+        v = 1 / (1 + self.i)
+        pv_of_pmt = self.PMT * (1 - v**self.n) / self.i
+        pv_of_fv = self.FV * v**self.n
+        return pv_of_pmt + pv_of_fv
 
-def solveForPV(i, PMT, FV, n):
-    v = 1 / (1 + i)
-    PV_OF_PMT = PMT * (1 - v**n) / i
-    PV_OF_FV = FV * v**n
-    return PV_OF_PMT + PV_OF_FV
+    def solve_for_fv(self):
+        fv_of_pmt = self.PMT * ((1 + self.i)**self.n - 1) / self.i
+        fv_of_pv = self.PV * (1 + self.i)**self.n
+        return fv_of_pmt + fv_of_pv
 
-def solveForFV(i, PMT, PV, n):
-    FV_OF_PMT = PMT * ((1 + i)**n - 1) / i
-    FV_OF_PV = PV * (1 + i)**n
-    return FV_OF_PMT + FV_OF_PV
+    def solve_for_pmt(self):
+        v = 1 / (1 + self.i)
+        pmt_of_pv = self.PV * self.i / (1 - v**self.n)
+        pmt_of_fv = self.FV * self.i / ((1 + self.i)**self.n - 1)
+        return pmt_of_pv + pmt_of_fv
 
-def solveForPMT(i, PV, FV, n):
-    v = 1 / (1 + i)
-    PMT_OF_PV = PV * i / (1 - v**n)
-    PMT_OF_FV = FV * i / ((1 + i)**n - 1)
-    return PMT_OF_PV + PMT_OF_FV
+    def solve_for_n(self):
+        v = 1 / (1 + self.i)
+        n = math.log((self.PMT - self.FV * self.i) / (self.PMT + self.PV * self.i)) / (2 * math.log(v))
+        return n
 
-def solveForN(i, PV, PMT, FV):
-    v = 1 / (1 + i)
-    n = math.log((PMT - FV * i) / (PMT + PV * i)) / (2 * math.log(v))
-    return n
-
-def solveForI(PV, PMT, FV, n):
-    for n in range(1, 10000):
-        i = n / 10000
-        if solveForPV(i, PMT, FV, n) - PV < 0.01:
-            return i
-        elif solveForFV(i, PMT, PV, n) - FV < 0.01:
-            return i
-        elif solveForPMT(i, PV, FV, n) - PMT < 0.01:
-            return i
-        elif solveForN(i, PV, PMT, FV) - n < 0.01:
-            return i
-        else:
-            errno.EINVAL
-        
-        
-            
-
-
-def TVM():
+    def solve_for_i(self):
+        for n in range(1, 10000):
+            i = n / 10000
+            self.i = i
+            if self.solve_for_pv() - self.PV < 0.01:
+                return i
+            elif self.solve_for_fv() - self.FV < 0.01:
+                return i
+            elif self.solve_for_pmt() - self.PMT < 0.01:
+                return i
+            elif self.solve_for_n() - self.n < 0.01:
+                return i
+            else:
+                errno.EINVAL
+    
+def tvm_calculator():
     print("Enter value or ? for param")
-    I = input("Interest rate: ")
-    PV = input("Present value: ")
-    PMT = input("Payment: ")
-    FV = input("Future value: ")
-    N = input("Number of periods: ")
-    F =  input("Frequency (e.g. '2M' or '3Y') : ")
+    i = float(input("Interest rate: ") or 0)
+    PV = float(input("Present value: ") or 0)
+    PMT = float(input("Payment: ") or 0)
+    FV = float(input("Future value: ") or 0)
+    n = float(input("Number of periods: ") or 0)
 
-    if I == "?":
-        I = solveForI(float(PV), float(PMT), float(FV), float(N))
-        print("Interest rate: ", I)
-    
-    elif PV == "?":
-        PV = solveForPV(float(I), float(PMT), float(FV), float(N))
-        print("Present value: ", PV)
-    
-    elif PMT == "?":
-        PMT = solveForPMT(float(I), float(PV), float(FV), float(N))
-        print("Payment: ", PMT)
+    calculator = TVM(i=i, PV=PV, PMT=PMT, FV=FV, n=n)
 
-    elif FV == "?":
-        FV = solveForFV(float(I), float(PMT), float(PV), float(N))
-        print("Future value: ", FV)
-    
-    elif N == "?":
-        N = solveForN(float(I), float(PV), float(PMT), float(FV))
-        print("Number of periods: ", N)
-    
+    if '?' in [i, PV, PMT, FV, n]:
+        if i == '?':
+            print("Interest rate: ", calculator.solve_for_i())
+        elif PV == '?':
+            print("Present value: ", calculator.solve_for_pv())
+        elif PMT == '?':
+            print("Payment: ", calculator.solve_for_pmt())
+        elif FV == '?':
+            print("Future value: ", calculator.solve_for_fv())
+        elif n == '?':
+            print("Number of periods: ", calculator.solve_for_n())
     else:
         print("Invalid input")
+
